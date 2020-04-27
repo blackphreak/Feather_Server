e4dcfd0d5ae9e4d1fd716202aba0a61467cd1cca
BlackPhreak <wi@hkwtc.org>
2020-04-27 17:13:28 +0800

    Revert "Revert "Merge Develop to Master""
    This reverts commit a380b98a84401cbd2a8d399d3c490cb64a032f47.

---
+++ Feather_Server/Entity/PlayerRelated/Skills/Players/Warrior_Skills.cs
@@ -0,0 +1,14 @@
+﻿using Feather_Server.ServerRelated;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Feather_Server.PlayerRelated.Skills.Players
+{
+    public class Warrior_Skills
+    {
+        //public static readonly PlayerSkill skills = new PlayerSkill[] {
+
+        //};
+    }
+}
